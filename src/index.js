635fc30567bcddf7707e508440d874f84dcff6a0
Aditya Tiwari <atiwari94255@gmail.com>
2024-07-30 18:16:07 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-add
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);;
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection Failed !!! ",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("ERROR: ",error);
+//             throw error
+//         })
+        
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Connected to port: ${process.env.PORT}`);
+//         })
+//     }
+//     catch(error){
+//         console.log("ERROR: ",error);
+//         throw error
+//     }
+// })()
